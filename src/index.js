e2061eb35349730820ceb174d8b3a01c656a523d
IFer <76787367+imash05@users.noreply.github.com>
2024-11-23 00:43:14 +0100

    cc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
+import global_it from "./translations/it/global.json";
+import global_en from "./translations/en/global.json";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+import HomePage from "./pages/HomePage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+i18next.init({
+  interpolation: {
+    escapeValue: false,
+  },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    it: {
+      global: global_it,
+    },
+  },
+});
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <GG />,  << When the site is under maintance >>
+  // },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <RouterProvider router={router} />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// TODO Puoi fornire una UX decisamente migliore di questa quando la tua app genera errori fornendone una tua ErrorBoundaryO errorElementsostegno sul tuo percorso.
